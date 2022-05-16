e5beee7c20776cf4ededc99275993b423eb6ad66
Benmuiruri <ben.muiruri13@gmail.com>
2022-05-16 16:21:09 +0300

    Install prop-types

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthContextProvider } from './store/auth-context';
 import './index.css';
 import App from './App';
 
@@ -8,7 +9,9 @@ const rootElement = document.getElementById('root');
 
 render(
   <BrowserRouter>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </BrowserRouter>,
   rootElement,
 );
