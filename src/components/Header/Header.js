e15a7fc9304fbbf58a4cc7970b9b98d5b424ee69
Benmuiruri <ben.muiruri13@gmail.com>
2022-05-18 12:02:27 +0300

    Style Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 import Navbar from './Navbar';
 // @ts-ignore
@@ -6,6 +8,10 @@ import classes from './Header.module.css';
 
 const Header = () => (
   <header className={classes['main-header']}>
+    <div className={classes.homeIcon}>
+      {' '}
+      <FontAwesomeIcon icon={faHouse} className={`${classes.faIcon}`} />
+    </div>
     <Navbar />
   </header>
 );
